9a6d80d90ff86ca2f0666b5cfa342d0c66628a74
Eric Bradley <131034082+ebradley0@users.noreply.github.com>
2025-01-16 10:13:25 -0500

    Adjusted pings to only notify for 2x powder, added command to check any event. Improved formatting

--- Bot.py
+++ Bot.py
@@ -1,11 +1,9 @@
 import discord
 import requests
-import json
-import time
 from datetime import datetime
 import os
 import asyncio
-import threading
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,43 +14,54 @@ bot_ready = asyncio.Event()
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-    channel = client.get_channel(1329327570478698547)
     bot_ready.set()
    
 
+embedded_format = discord.Embed(
+    title = "Dwarven Mines Event",
+    description = "Current Event: ",
+    color = discord.Color.green()
+)
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("$event"):
+        CurrentDwarvenEvent, CurrentDwarvenTimeStamp, EndingDwarvenTimeStamp, is_double = check_event()
+        embedded_format.description = "Current Event: " + str(prettier_event_name(CurrentDwarvenEvent)) + " ends at " + "<t:" + str(EndingDwarvenTimeStamp) + ":F>"
+        channel = message.channel
+        await channel.send(embed=embedded_format)
+        print("Event Command Used")
 
 async def main_script():
     await bot_ready.wait()
     channel = client.get_channel(1329327570478698547)
     pinged = False
     EventUpdated = False
-    print(channel)
     while active:
 
 
-            if EventUpdated == False:
-                CurrentDwarvenEvent, CurrentDwarvenTimeStamp, EndingDwarvenTimeStamp = check_event()
+            if not EventUpdated:
+                CurrentDwarvenEvent, CurrentDwarvenTimeStamp, EndingDwarvenTimeStamp, is_double = check_event()
                 EventUpdated = True
                 #Send message in discord with data
-                print("Event Updated")
-                print("Current Event: " + CurrentDwarvenEvent)
+               
+                print("Current Event: " + str(CurrentDwarvenEvent))
             if datetime.now().timestamp() >= EndingDwarvenTimeStamp:
                 EventUpdated = False
                 pinged = False
+                print("======================Event Ended======================")
 
-            if CurrentDwarvenEvent == "DOUBLE_POWDER" and pinged == False:
-                
+            if CurrentDwarvenEvent == "DOUBLE_POWDER" and not pinged:
+                embedded_format.description = "Current Event: " + str(prettier_event_name(CurrentDwarvenEvent)) + " ends at " + "<t:" + str(EndingDwarvenTimeStamp) + ":F>"
+                embedded_format.color = discord.Color.teal()
                 pinged = True
+                await channel.send(embed=embedded_format)
                 print("2x Powder")
-                time_diff = EndingDwarvenTimeStamp - datetime.now().timestamp()
-                await channel.send("2x Powder" + " ends at" +"<t:" + str(EndingDwarvenTimeStamp) + ":F>")
                 await channel.send("<@&" + str(1329338766938341428) +">")
-              
-             
-            else:
-                print("Event not changed, waiting 60 seconds")
-                await asyncio.sleep(60)
+            print("Checking Event in 60 seconds")
+            await asyncio.sleep(60)
                 
             
                 
@@ -62,24 +71,48 @@ async def main():
 
 
 
-    
+def prettier_event_name(event):
+    match event:
+        case "DOUBLE_POWDER":
+            return "Double Powder"
+        case "BETTER_TOGETHER":
+            return "Better Together"
+        case "MITHRIL_GOURMAD":
+            return "Mithril Gourmad"
+        case "GONE_WITH_THE_WIND":
+            return "Gone with the Wind"
+        case "RAFFLE":
+            return "Raffle"
+        case "GOBLIN_RAID":
+            return "Goblin Raid"
 
 
 
 def check_event():
+    print("Checking Event")
     rq = requests.get("https://api.soopy.dev/skyblock/chevents/get")
     data = rq.json()
-    formatted_data = json.dumps(data, indent=2)
     DwarvenData = data["data"]["event_datas"]["DWARVEN_MINES"]
     CurrentDwarvenEvent = next(iter(DwarvenData))
     CurrentDwarvenTimeStamp = round(int(DwarvenData[CurrentDwarvenEvent]["starts_at_min"]) / 1000)
     EndingDwarvenTimeStamp = round(int(DwarvenData[CurrentDwarvenEvent]["ends_at_max"]) / 1000) # convert to seconds
     
+    for event in DwarvenData:
+        if event == "DOUBLE_POWDER":
+            CurrentDwarvenEvent = next(iter(DwarvenData))
+            EndingDwarvenTimeStamp = round(int(DwarvenData[event]["ends_at_max"]) / 1000) # convert to seconds
+            is_double = True
+        else:
+            CurrentDwarvenEvent = next(iter(DwarvenData))
+            EndingDwarvenTimeStamp = round(int(DwarvenData[event]["ends_at_max"]) / 1000)
+            print(event + "Ends at " + datetime.fromtimestamp(EndingDwarvenTimeStamp).strftime('%Y-%m-%d %H:%M:%S'))
+            is_double = False
+            
     
     
     #CHData = data["data"]["event_datas"]["CRYSTALL_HOLLOWS"]
 
     
-    return CurrentDwarvenEvent, CurrentDwarvenTimeStamp, EndingDwarvenTimeStamp
+    return CurrentDwarvenEvent, CurrentDwarvenTimeStamp, EndingDwarvenTimeStamp, is_double
 
 asyncio.run(main())
